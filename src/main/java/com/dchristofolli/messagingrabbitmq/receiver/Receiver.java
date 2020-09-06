c2b254b6a88a09e718b73baa59064f1be4787860
dchristofolli <danielchristofolli@gmail.com>
2020-09-05 22:10:48 -0300

    salvando entidade recebida no mongo

--- src/main/java/com/dchristofolli/messagingrabbitmq/receiver/Receiver.java
+++ src/main/java/com/dchristofolli/messagingrabbitmq/receiver/Receiver.java
@@ -1,6 +1,7 @@
 package com.dchristofolli.messagingrabbitmq.receiver;
 
 import com.dchristofolli.messagingrabbitmq.model.UserEntity;
+import com.dchristofolli.messagingrabbitmq.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -8,10 +9,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class Receiver {
+    private final UserRepository userRepository;
     private static final Logger log = LoggerFactory.getLogger(Receiver.class);
+
+    public Receiver(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @RabbitListener(queues = "${rabbitmq.specificQueueName}")
     public void receiveMessage(final UserEntity userEntity) {
         log.info("Received message as specific class: {}", userEntity);
+        userRepository.save(userEntity);
     }
+
+
 }
